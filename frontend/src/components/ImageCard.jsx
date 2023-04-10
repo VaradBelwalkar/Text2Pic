54a36ae063c17c90477e5a9db4edbe5ab2e22c60
SwagPack <belwalkarvarad@gmail.com>
2023-04-10 23:15:42 +0530

    add download button after image

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -1,8 +1,11 @@
 
 const ImageCard = ({ image }) => {
     return (
+        <>
         <img className="w-full rounded shadow-sm" src={image} alt="" />
-    )
+        <ImageDownloadButton imageUrl={image} />
+        </>
+        )
 }
 
 export default ImageCard
