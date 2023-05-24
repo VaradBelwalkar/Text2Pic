a05c4a1dbdf8b6dfea67c3430405fcfb57bd9e12
SwagPack <belwalkarvarad@gmail.com>
2023-05-24 20:05:53 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,1 +1,9 @@
-some content
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./file.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+function signup()
