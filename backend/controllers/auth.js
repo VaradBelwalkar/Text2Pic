e0ad00e030c548c86c1c0d67ccf4f25cd23b8873
SwagPack <belwalkarvarad@gmail.com>
2023-05-24 20:28:33 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,9 +1,52 @@
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./file.db', (err) => {
-  if (err) {
-    return console.error(err.message);
+const signup = async (req, res) => {
+    const { username, password } = req.body;
+  
+    // Check if username or password contain SQL queries
+    const isUsernameSafe = !containsSQLQuery(username);
+    const isPasswordSafe = !containsSQLQuery(password);
+  
+    if (!isUsernameSafe || !isPasswordSafe) {
+      return res.status(400).json({ error: 'Invalid input.' });
+    }
+  
+    // Check if the username already exists in the database
+    db.get("SELECT id FROM userlist WHERE username = ?", [username], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: 'Database error.' });
+      }
+  
+      if (row) {
+        return res.status(409).json({ error: 'Username already exists.' });
+      }
+  
+      // Insert the new user into the database
+      db.run("INSERT INTO userlist (username, password) VALUES (?, ?)", [username, password], function (err) {
+        if (err) {
+          console.error(err.message);
+          return res.status(500).json({ error: 'Database error.' });
+        }
+  
+        // Return a success response
+        return res.status(200).json({ message: 'Signup successful.' });
+      });
+    });
+  };
+  
+  // Helper function to check if a string contains potential SQL queries
+  function containsSQLQuery(text) {
+    const sqlKeywords = ['SELECT', 'UPDATE', 'DELETE', 'INSERT', 'DROP', 'CREATE'];
+    const regex = new RegExp(`\\b(${sqlKeywords.join('|')})\\b`, 'i');
+    return regex.test(text);
   }
-  console.log('Connected to the in-memory SQlite database.');
-});
 
-function signup()
+
+const login = async (req,res)=>{
+    db.get("SELECT * FROM userlist WHERE username=? && password=?",[username,password],(err,row)=>{
+        if (!row){
+            return res.status(404).json({message: "wrong authentication"});
+        }
+        //Return appropriate response here
+        return res.status(200).json({message: "authentication successful"});
+    });
+}
