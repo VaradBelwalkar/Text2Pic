ee8abfae1a7085047b9fee9c7f71f187a6aeb4e6
SwagPack <belwalkarvarad@gmail.com>
2023-05-24 22:36:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import ImageResults from "./components/ImageResults"
+import signup from "./components/signup"
 function App() {
   const [listImages, setListImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  
-  
+  token
+  if (token.length > 0{
   return (
     <div>
       <Header setListImages={setListImages} setIsLoading={setIsLoading} token={token}/>
       <ImageResults listImages={listImages} isLoading={isLoading} />
     </div>
-  )
+  )}
+else{
+return (
+<div>
+  <signup/>
+ </div>
+
+
+)
+}
 }
 
 export default App
