0b9372037b5732ef83b0b319cea69bc65f8f810a
SwagPack <belwalkarvarad@gmail.com>
2023-05-24 19:56:27 +0530

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { generateImage } = require("../controllers/openaiControllers");
-
+const {signuppage,loginpage} = require("../controllers/pages");
+const {signup,login} = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/images", generateImage)
-
+router.get("/signup", signuppage)
+router.get("/login",loginpage)
+router.post("signup",signup)
+router.post("login",login)
 module.exports = router;
